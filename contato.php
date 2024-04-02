eacca76dc88f8f6d83f04e2493c07cbe45aace21
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-04-01 22:19:02 -0300

    Initial commit

---
+++ contato.php
@@ -0,0 +1,37 @@
+<?php include("cabecalho.php")?>
+<?php include("menu.php")?>
+<main class="bg-light row pe-3 ps-3">
+            <div class="row">
+                <h2 class="mt-4 h1">Entre em contato</h2>
+                <hr>
+                <div class="col">
+                    <form action="">
+                        <div class="pt-5">
+                            <div class="inputbox mb-2">
+                                <input class="bg-dark-subtle border-light-subtle" type="text" name="nome" id="nome" placeholder="Nome">
+                                <label for="nome"></label>
+                            </div>
+                            <div class="inputbox mb-2">
+                                <input class="bg-dark-subtle border-light-subtle" type="email" name="email" id="email" placeholder="E-mail">
+                                <label for="email"></label>
+                            </div>
+                            <div class="inputbox mb-2">
+                                <input class="bg-dark-subtle border-light-subtle" type="text" name="assunto" id="assunto" placeholder="Assunto">
+                                <label for="assunto"></label>
+                            </div>
+                            <div class="inputbox mb-2">
+                                <textarea class="bg-dark-subtle border-light-subtle" name="mensagem" id="mensagem" placeholder="Mensagem"></textarea>
+                                <label for="mensagem"></label>
+                            </div>
+                            <div class="inputbox text-center">
+                            <button type="submit">Enviar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14718.882396362662!2d-47.32584758158236!3d-22.738624081719685!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c89bcbad3d5da7%3A0x33d5e2d5266bc406!2sEdwiges%20Pizza%20e%20Bistr%C3%B4%20%7C%20Pizzaria%20em%20Americana!5e0!3m2!1spt-BR!2sbr!4v1712018992470!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+</main>
+<?php include("rodape.php")?>
---
+++ index.php
@@ -0,0 +1,60 @@
+        <?php include("cabecalho.php") ?>
+        <?php include("menu.php") ?>
+        <main class="bg-light row pe-3 ps-3">
+            <div class="row">
+                <h2 class="mt-4 h1">A melhor pizza da região</h2>
+                <hr>
+                <div class="col text-center">
+                    <img src="calabreza.png" alt="Calabresa">
+                    <p>Calabresa</p>
+                </div>
+
+                <div class="col text-center">
+                    <img src="calabreza.png" alt="Calabresa">
+                    <p>Calabresa</p>
+                </div>
+
+                <div class="col text-center">
+                    <img src="calabreza.png" alt="Calabresa">
+                    <p>Calabresa</p>
+                </div>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h3>Nossos sabores</h3>
+                    <p class="sabores"> À Moda da Casa  <br>
+                        Aliche <br>
+                        Atum <br>
+                        Baiana <br>
+                        Bauru <br>
+                        Canadense <br>
+                        Catupiry <br>
+                        Calabresa <br>
+                        À Moda da Casa  <br>
+                        Aliche <br>
+                        Atum <br>
+                        Baiana <br>
+                        Bauru <br>
+                        Canadense <br>
+                        Catupiry <br>
+                        Calabresa
+                    </p>
+                </div><!-- coluna -->
+                <div class="col">
+                    <h3>Uma casa com história</h3>
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="historia.png" class="img-fluid" alt="historia">
+                        </div>
+                        <div class="col">
+                            <p>Muito famosa em toda a região por sua excelência na qualidade, a nossa pizzzaria tem mais de 25 anos de história.</p>
+                            <p>
+                                <a href="quem-somos.php" class="link-destaque">Conheça</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- linha -->
+        </main>
+        <?php include("rodape.php") ?>
